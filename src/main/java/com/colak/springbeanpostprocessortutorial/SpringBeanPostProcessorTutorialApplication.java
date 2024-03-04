d71fb5ad4b990034416467cf76748a5a405f5cd4
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-04 09:53:50 +0300

    rename class

--- src/main/java/com/colak/springbeanpostprocessortutorial/SpringBeanPostProcessorTutorialApplication.java
+++ src/main/java/com/colak/springbeanpostprocessortutorial/SpringBeanPostProcessorTutorialApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBeanpostprocessorTutorialApplication {
+public class SpringBeanPostProcessorTutorialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBeanpostprocessorTutorialApplication.class, args);
+		SpringApplication.run(SpringBeanPostProcessorTutorialApplication.class, args);
 	}
 
 }
